5cf3e67b9ea229f7aa1e74e8d1f9f84f0d473ecb
Shahzaib <logical9242@gmail.com>
2025-01-02 12:19:26 +0500

    posts crud operations

--- dashboards/forms.py
+++ dashboards/forms.py
@@ -8,3 +8,17 @@ class CategoryForm(forms.ModelForm):
     class Meta:
         model = Category
         fields = '__all__'
+        
+
+class BlogPostForm(forms.ModelForm):
+    class Meta:
+        model = Blog
+        fields = ('title', 'category', 'featured_image', 'short_description', 'blog_body', 'status', 'is_featured')
+
+
+
+
+class EditUserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'first_name', 'last_name', 'is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')
