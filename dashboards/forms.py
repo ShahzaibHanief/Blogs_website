2d1b750c8c62362ddc9d08096647d452ad39eb98
Shahzaib <logical9242@gmail.com>
2025-01-02 13:31:37 +0500

    User Crude operations and permissions

--- dashboards/forms.py
+++ dashboards/forms.py
@@ -22,3 +22,11 @@ class EditUserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'first_name', 'last_name', 'is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')
+
+
+
+class AddUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'first_name', 'last_name', 'is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')
+
